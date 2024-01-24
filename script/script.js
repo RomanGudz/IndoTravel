b3c3c38d71567a12bfe12af75f47f2d414066644
Roman Gudz <alien31roman@gmail.com>
2024-01-24 22:42:04 +0300

    module_03_lessosn_02-hardtask-01

--- script/script.js
+++ script/script.js
@@ -2,21 +2,31 @@
 
 const timerPage = () => {
   const deadline = document.querySelector('.hero__timer');
-  deadline.setAttribute('data-timer-deadline', '2024/02/10 ')
+  deadline.setAttribute('data-timer-deadline', '2024/01/25 22:31')
   const getDeadline = deadline.getAttribute('data-timer-deadline');
   const discountText = document.querySelector('.hero__text')
   const timerDay = document.getElementsByClassName('timer__item_days');
   const timerHours = document.getElementsByClassName('timer__item_hours');
   const timerMinutes = document.getElementsByClassName('timer__item_minutes');
   const timer = () => {
-    const dateStop = new Date(getDeadline).getTime();
+    const dateStop = () => {
+      const date = new Date(getDeadline).getTime();
+      const timeZone = (new Date(getDeadline).getTimezoneOffset() / 60) + 3
+      if (0 === timeZone) {
+        return date
+      } else {
+        console.log(timeZone);
+        return ((1000 * 60 * 60) * timeZone) + date;
+      }
+    };
     const dateNow = Date.now();
-    const stopTimer = dateStop - dateNow;
+    const stopTimer = dateStop() - dateNow;
 
     const days = Math.floor(stopTimer / 1000 / 60 / 60 / 24);
     const hours = Math.floor(stopTimer / 1000 / 60 / 60 % 24);
     const minutes = Math.floor(stopTimer / 1000 / 60 % 60);
-    return { stopTimer, days, hours, minutes }
+    const seconds = Math.floor(stopTimer / 1000 % 60)
+    return { stopTimer, days, hours, minutes, seconds }
   };
 
   const dayTitle = number => {
@@ -64,6 +74,21 @@ const timerPage = () => {
       return 'минут';
     }
   };
+  const secondTitile = number => {
+    if (number > 10 && [11, 12, 13, 14].includes(number % 100)) {
+      return 'секунд';
+    };
+    const lastnumber = number % 10;
+    if (lastnumber === 1) {
+      return 'секунда'
+    };
+    if ([2, 3, 4].includes(lastnumber)) {
+      return 'секунды';
+    }
+    if ([5, 6, 7, 8, 9, 0].includes(lastnumber)) {
+      return 'секунд';
+    }
+  };
 
   const controlNumber = number => {
     if (number > 10) {
@@ -85,6 +110,14 @@ const timerPage = () => {
       clearTimeout(counterTimer);
       deadline.remove()
       discountText.remove()
+    }
+    if (startTimer.days < 1) {
+      timerDay[0].children[0].textContent = controlNumber(startTimer.hours)
+      timerDay[0].children[1].textContent = hourTitle(startTimer.hours);;
+      timerHours[0].children[0].textContent = controlNumber(startTimer.minutes);
+      timerHours[0].children[1].textContent = minuteTitle(startTimer.minutes);
+      timerMinutes[0].children[0].textContent = controlNumber(startTimer.seconds);
+      timerMinutes[0].children[1].textContent = secondTitile(startTimer.seconds);
     }
   }
 
