001715601383ce4876758b68f35beaefd17834fc
Roman Gudz <alien31roman@gmail.com>
2024-01-25 21:10:01 +0300

    module_03_lessons_03-hardtask

--- script/module/headerMenu.js
+++ script/module/headerMenu.js
@@ -1,14 +1,23 @@
 const btnHeaderMenu = document.querySelector('.header__menu-button');
 const headerMenu = document.querySelector('.header__menu');
 const itemsMenu = document.querySelectorAll('.header__item');
-const main = document.querySelector('.section');
 
-btnHeaderMenu.addEventListener('click', () => {
-  headerMenu.classList.toggle('header__menu_active');
-});
-itemsMenu.forEach(item => {
-  item.addEventListener('click', () => {
+// btnHeaderMenu.addEventListener('click', () => {
+//   headerMenu.classList.toggle('header__menu_active');
+// });
+// itemsMenu.forEach(item => {
+//   item.addEventListener('click', () => {
+//     headerMenu.classList.toggle('header__menu_active');
+//   })
+// })
+
+document.addEventListener('click', (event) => {
+  const isMenuClicked = headerMenu.contains(event.target);
+  const isButtonClicked = btnHeaderMenu.contains(event.target);
+
+  if (!isMenuClicked && !isButtonClicked) {
     headerMenu.classList.toggle('header__menu_active');
-  })
-})
-
+  } else {
+    headerMenu.classList.toggle('header__menu_active');
+  }
+});
