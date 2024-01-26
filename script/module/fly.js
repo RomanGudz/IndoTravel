935c328f38d5b35dd8bc0f5d30a7e6a158c85b00
Roman Gudz <alien31roman@gmail.com>
2024-01-27 00:05:50 +0300

    module_03_lessons_04-hardtask

--- script/module/fly.js
+++ script/module/fly.js
@@ -7,7 +7,8 @@ width: 50px;
 height: 50px;
 right: 0px;
 bottom: 0px;
-pointer-events: none;
+pointer-events: non;
+rotate: 0deg;
 background: url('img/airplane.svg') center/contain no-repeat;
 `;
 
@@ -16,8 +17,19 @@ const calcpositionFly = () => {
   const maxScroll = docElem.scrollHeight - docElem.clientHeight;
   const percentScroll = (window.scrollY * 100) / maxScroll;
   const height = maxHeight * (percentScroll / 100);
+  console.log('height: ', percentScroll);
   fly.style.transform = `translateY(-${height}px)`;
 
+  if (Math.round(height) === maxHeight) {
+    console.log(fly.style.transform);
+    fly.style.rotate = '180deg';
+    fly.style.transform = `translateY(${height}px)`;
+  }
+  // if (fly.style.transform >= 0) {
+  //   console.log('-1');
+  //   fly.style.rotate = '180deg';
+  //   fly.style.transform = `translateY(${height - maxHeight}px)`;
+  // }
 };
 calcpositionFly()
 document.body.append(fly);
